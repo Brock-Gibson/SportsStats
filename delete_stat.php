922eece26e2057828e42d7d7e273a0f474b57f91
Brock Gibson <bngd58@mail.missouri.edu>
2017-12-04 09:50:32 -0600

    Completed delete functionality /Brock

--- delete_stat.php
+++ delete_stat.php
@@ -1,4 +1,9 @@
-<?
+<?php
+        require ('db_credentials.php');
+	    require ('web_utils.php');
+
+        $stylesheet = 'sportsstats.css';
+
         $id = $_POST['id'];
 
 		$message = "";
@@ -7,31 +12,31 @@
 			$message = "No task was specified to delete.";
 		} else {
 			// Create connection
-			require('db_credentials.php');
+
 			$mysqli = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
 			if ($mysqli->connect_error) {
 				$message = $mysqli->connect_error;
 			} else {
 				$id = $mysqli->real_escape_string($id);
-				$sql = "DELETE FROM GAMES WHERE id = $id";
+				$sql = "DELETE FROM GAMES WHERE GameID = $id";
 				if ( $result = $mysqli->query($sql) ) {
 					redirect("index.php");
 				} else {
 					print generatePageHTML("Stats (Error)", generateErrorPageHTML($conn->error . " using SQL: $sql"), $stylesheet);
+                    exit;
 				}
 				$mysqli->close();
 			}
 		}
 
+function generateErrorPageHTML($error) {
+	$html = <<<EOT
+<h1>Stats</h1>
+<p>An error occurred: $error</p>
+<p><a class='statButton' href='stat_Form.html'>Add Stat</a><a class='statButton' href='index.php'>View Stats</a></p>
+EOT;
 
-    function generateErrorPageHTML($error) {
-        $html = <<<EOT
-    <h1>Stats</h1>
-    <p>An error occurred: $error</p>
-    <p><a class='statButton' href='stat_Form.html'>Add Stat</a><a class='statButton' href='index.php'>View Stats</a></p>
-    EOT;
-
-        return $html;
-        }
+	return $html;
+	}
 ?>
